1f7a4d46cabc8a4854b5878acd6472ecba596c6c
Itirapleguisdaw1 <itirapleguisdaw1@gmail.com>
2021-03-26 20:19:23 +0100

    Añadido comentario a la clase PruebaGitEclipse

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -4,6 +4,15 @@ import prueba.modelo.Saludo;
 
 public class PruebaGitEclipse {
 
+	
+	/**
+	 * PUNTO DE ENTRADA A LA APLICACIÓN
+	 * @author Iñaki Ti Unai Pe Enrique La
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
 	public static void main(String[] args) {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Juan"));
